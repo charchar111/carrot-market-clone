dddb390e97eae327015c8742475a9735508c95a6
charchar111 <3hjab@naver.com>
2023-12-13 09:18:11 +0900

    lecture:#11.6

---
+++ src/pages/api/products/[id]/favorite.ts
@@ -0,0 +1,39 @@
+import client from "@/libs/server/client";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@/libs/server/withSession";
+import { use } from "react";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>,
+): Promise<any> {
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+
+  console.log("id, user", id, user);
+  //  유저 아이디 체크
+  if (!id || !user) return res.status(401).json({ ok: false });
+  const aleadyExists = await client.favorite.findFirst({
+    where: {
+      productId: +id.toString(),
+      userId: +id.toString(),
+    },
+  });
+  if (aleadyExists) {
+    await client.favorite.delete({ where: { id: aleadyExists.id } });
+  } else {
+    client.favorite.create({
+      data: {
+        userId: user.id,
+        productId: +id.toString(),
+      },
+    });
+  }
+
+  return res.status(200).json({ ok: true });
+}
+
+export default withApiSession(withHandler({ methods: ["POST"], handler }));
